79db830c12f752aa90d9425c8c2e583f10300242
David Gaspar <davidgaspar.dev@gmail.com>
2019-06-22 01:54:51 -0300

    Installed typescript

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * dList
+ * 
+ * @author David Gaspar
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import { app as runApp } from './App';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, runApp);
